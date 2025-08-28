39b1d637218d9587abf17303962fee542437138f
Shailesh <jshailesh1999@example.com>
2025-08-28 16:05:07 +0530

    Initial commit for ScholarHat demo

---
+++ src/main/java/com/scholarhat/demo/ScholarhatDemoApplication.java
@@ -0,0 +1,13 @@
+package com.scholarhat.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScholarhatDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScholarhatDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scholarhat/demo/ScholarhatDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scholarhat.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScholarhatDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
